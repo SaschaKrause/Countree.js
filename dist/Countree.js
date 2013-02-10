58616d4d383ea0a06e0d7fb9a82818a093a68bb9
Kai Hoelscher <myfatbackup@yahoo.de>
2013-02-10 16:21:28 +0100

    Forgot to run grunt

--- dist/Countree.js
+++ dist/Countree.js
@@ -106,15 +106,7 @@
          * @private
          */
         function countDirectionIs(countDirection) {
-            validateCountDirection(countDirection);
             return that.options.direction === countDirection;
-        }
-
-        /**
-         * @private
-         */
-        function validateCountDirection() {
-            return; // TODO implement
         }
 
         /**
